490bcdd515ed4a94d0321e987db1e50f82f46e48
awera <aw3ra@proton.me>
2023-10-11 06:47:38 +1100

    added some stuff

--- src/modules/audio_content/audio_io.py
+++ src/modules/audio_content/audio_io.py
@@ -1,5 +1,8 @@
 import wave
 import audioop
+import pvporcupine
+import alsaaudio
+import numpy as np
 from ctypes import *
 from contextlib import contextmanager
 import pyaudio
@@ -12,7 +15,9 @@ import openai
 
 # Configuration for Eleven Labs
 os.environ['PATH'] += r";C:\Program Files\ffmpeg-master-latest-win64-gpl\bin"
-set_api_key(os.environ["ELEVEN_LABS_KEY"])
+OPENAI_KEY = os.environ["OPEN_AI_KEY"]
+access_key = os.environ["PICOVOICE_KEY"]
+
 
 # Error handler for ALSA
 ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
@@ -31,67 +36,90 @@ def noalsaerr():
 
 
 class AudioRecorder:
-    def __init__(self, silence_threshold=900, silence_duration=1, min_audio_duration=1):
+    def __init__(self, silence_threshold=2550, silence_duration=5, min_audio_duration=3):
         self.silence_threshold = silence_threshold
         self.silence_duration = silence_duration
         self.min_audio_duration = min_audio_duration
-        self.OUTPUT_FILENAME = "./src/audio_content/audio.wav"
+        self.OUTPUT_FILENAME = "test.wav"
 
     def record(self):
-        FORMAT = pyaudio.paInt16
-        CHANNELS = 1
-        RATE = 44100
-        CHUNK = 4096
-        
-        with noalsaerr():
-            audio = pyaudio.PyAudio()
+            # PV Porcupine initialization
+            handle = pvporcupine.create(access_key=access_key, keyword_paths=['wakewords.ppn'])
+            inp = alsaaudio.PCM(
+                alsaaudio.PCM_CAPTURE,
+                channels=2,
+                rate=handle.sample_rate,
+                format=alsaaudio.PCM_FORMAT_S32_LE,
+                periodsize=handle.frame_length,
+                device='plughw:1,0'
+            )
 
-        stream = audio.open(format=FORMAT, channels=CHANNELS,
-                            rate=RATE, input=True,
-                            frames_per_buffer=CHUNK,
-                            input_device_index=1)
+            audio_buffer = []
+            frames = []  # to save audio data
+            silence_frames = 0
+            recording_started = False
 
-        frames = []
-        silence_frames = 0
-        recording_started = False
+            def get_next_audio_frame():
+                nonlocal audio_buffer
+                l, pcm_data = inp.read()
+                if l <= 0:
+                    return None
 
-        while True:
-            data = stream.read(CHUNK)
-            rms = audioop.rms(data, 2)
+                pcm_32 = np.frombuffer(pcm_data, dtype=np.int32)
+                pcm_16 = (pcm_32 >> 16).astype(np.int16)
+                pcm_mono = [(pcm_16[i] + pcm_16[i + 1]) // 2 for i in range(0, len(pcm_16), 2)]
 
-            if rms > self.silence_threshold:
-                if not recording_started:
-                    print("Recording started...")
-                    recording_started = True
-                frames.append(data)
-                silence_frames = 0
-            elif recording_started:
-                frames.append(data)
-                silence_frames += 1
-                if silence_frames * CHUNK / RATE > self.silence_duration:
-                    break
+                amplification_factor = 3
+                amplified_mono = [int(i * amplification_factor) for i in pcm_mono]
+                audio_buffer += amplified_mono
 
-        stream.stop_stream()
-        stream.close()
-        audio.terminate()
+                if len(audio_buffer) >= handle.frame_length:
+                    frame = audio_buffer[:handle.frame_length]
+                    audio_buffer = audio_buffer[handle.frame_length:]
+                    return frame
+                return None
 
-        if len(frames) * CHUNK / RATE > self.min_audio_duration:
-            filename = self.OUTPUT_FILENAME
-            with wave.open(filename, 'wb') as wf:
-                wf.setnchannels(CHANNELS)
-                wf.setsampwidth(audio.get_sample_size(FORMAT))
-                wf.setframerate(RATE)
-                wf.writeframes(b''.join(frames))
-            return filename
-        else:
-            print("Recording was too short, discarding...")
-            return None
+            print("Listening for wake word...")
+            while True:
+                frame = get_next_audio_frame()
+                if frame:
+                    keyword_index = handle.process(frame)
+                    if keyword_index >= 0:
+                        print("Wake word detected! Starting recording...")
+                        recording_started = True
 
-    def transcribe(OUTPUT_FILENAME):
-        audio_file= open(OUTPUT_FILENAME, "rb")
-        openai.api_key = os.environ["OPEN_AI_KEY"]
+                    if recording_started:
+                        rms = np.sqrt(np.mean(np.square(frame)))
+                        print(f"RMS value: {rms}")  # Debug print
+                        if rms > self.silence_threshold:
+                            frames.append(np.array(frame, dtype=np.int16).tobytes())
+                            silence_frames = 0
+                        else:
+                            silence_frames += 1
+                            print(f"Silent frames: {silence_frames}")  # Debug print
+
+
+                        if silence_frames * handle.frame_length / handle.sample_rate > self.silence_duration:
+                            print("Recording finished.")
+                            break
+
+            if len(frames) * handle.frame_length / handle.sample_rate > self.min_audio_duration:
+                filename = self.OUTPUT_FILENAME
+                with wave.open(filename, 'wb') as wf:
+                    wf.setnchannels(1)
+                    wf.setsampwidth(2)
+                    wf.setframerate(handle.sample_rate)
+                    wf.writeframes(b''.join(frames))
+                return filename
+            else:
+                print("Recording was too short, discarding...")
+                return None
+    
+    def transcribe(self, audio_filename):   # added audio_filename as an argument
+        audio_file= open(audio_filename, "rb")
+        openai.api_key = OPENAI_KEY
         transcript = openai.Audio.transcribe("whisper-1", audio_file)
-        return transcript
+        return transcript.text
 
 
 class AudioPlayer:
@@ -101,6 +129,7 @@ class AudioPlayer:
 
     def generate_and_play(self, text):
         audio_time_start = time.time()
+        set_api_key = "cbbc88987232bc4e6af5db6015931420"
         audio = generate(text="--" + text, voice=self.voice)
         audio_time_end = time.time()
         print("Audio time: " + str(audio_time_end - audio_time_start))
@@ -116,9 +145,9 @@ class AudioPlayer:
 
 # Usage example:
 if __name__ == "__main__":
+    print("started")
     recorder = AudioRecorder()
-    recorded_file = recorder.record()
+    recorder.record()
 
-    if recorded_file:
-        player = AudioPlayer()
-        player.generate_and_play("Recording saved!")
+    print(recorder.transcribe("test.wav"))
+
--- src/modules/audio_content/find_mics.py
+++ src/modules/audio_content/find_mics.py
@@ -1,8 +1,59 @@
-import pyaudio
+import pvporcupine
+import alsaaudio
+import struct
+import audioop
+import numpy as np
+import os
 
-p = pyaudio.PyAudio().get_device_info_by_host_api_device_index(0, 0)
-print(p)
-for i in range(p.get_device_count()):
-    dev = p.get_device_info_by_index(i)
-    print((i, dev['name'], dev['maxInputChannels']))
-    
+# AccessKey obtained from Picovoice Console (https://console.picovoice.ai/)
+access_key = os.environ["PICOVOICE_KEY"]
+
+handle = pvporcupine.create(access_key=access_key, keyword_paths=['wakewords.ppn'])
+print(handle.sample_rate, handle.frame_length)
+
+inp = alsaaudio.PCM(
+    alsaaudio.PCM_CAPTURE,
+    channels=2,
+    rate=handle.sample_rate,
+    format=alsaaudio.PCM_FORMAT_S32_LE,
+    periodsize=handle.frame_length,
+    device='plughw:1,0'
+)
+
+
+audio_buffer = []
+
+def get_next_audio_frame():
+    global audio_buffer
+    l, pcm_data = inp.read()
+    if l <= 0:
+        return None
+    pcm_32 = np.frombuffer(pcm_data, dtype=np.int32)
+    pcm_16 = (pcm_32 >> 16).astype(np.int16)
+    # Convert stereo to mono (since you mentioned stereo input earlier)
+    pcm_mono = [(pcm_16[i] + pcm_16[i + 1]) // 2 for i in range(0, len(pcm_16), 2)]
+
+    # Amplify the mono signal
+    amplification_factor = 3
+    amplified_mono = [int(i * amplification_factor) for i in pcm_mono]
+
+    # Add the amplified audio to the buffer
+    audio_buffer += amplified_mono
+
+    # If buffer has enough samples, pop them and return
+    if len(audio_buffer) >= handle.frame_length:
+        frame = audio_buffer[:handle.frame_length]
+        audio_buffer = audio_buffer[handle.frame_length:]
+        return frame
+    return None
+
+print("Started listening")
+while True:
+    frame = get_next_audio_frame()
+    if frame:
+        keyword_index = handle.process(frame)
+        if keyword_index >= 0:
+            print("hello!")
+
+
+
